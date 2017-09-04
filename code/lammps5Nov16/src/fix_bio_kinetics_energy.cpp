e258a3c79616cfa4d4922ccb76e2d59e843921cd
shelllbw <libowen96@hotmail.com>
2017-09-04 14:57:01 +0100

    Add yield term in growth model

--- code/lammps5Nov16/src/fix_bio_kinetics_energy.cpp
+++ code/lammps5Nov16/src/fix_bio_kinetics_energy.cpp
@@ -260,8 +260,8 @@ double FixKineticsEnergy::biomass(int i) {
         if (1.2 * bacMaint < qCat) {
           double metCoeff = catCoeff[t][nu] * invYield + anabCoeff[t][nu];
 
-          //mu = gYield[t][pos] * (qMet - bacMaint);
-          mu = qMet - bacMaint;
+          mu = gYield[t][pos] * (qMet - bacMaint);
+          //mu = qMet - bacMaint;
           mass = mu * rmass[i];
           consume = mass  * metCoeff;
         } else if (qCat <= 1.2 * bacMaint && bacMaint <= qCat) {
@@ -285,8 +285,8 @@ double FixKineticsEnergy::biomass(int i) {
   }
 
   if (1.2 * bacMaint < qCat) {
-    //mu = gYield[t][pos] * (qMet - bacMaint);
-    mu = qMet - bacMaint;
+    mu = gYield[t][pos] * (qMet - bacMaint);
+    //mu = qMet - bacMaint;
     mass = mu * rmass[i];
   } else if (qCat <= 1.2 * bacMaint && bacMaint <= qCat) {
     mass = 0;
